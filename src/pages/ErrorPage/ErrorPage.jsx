6681a1c103b71e15dcbef780bc83fdec6a7cb7b2
Anik98B <anikbhowmik0188@gmail.com>
2024-04-09 01:12:11 +0600

    added errorpage component

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    return (
+        <div className="text-center mt-44">
+            <h1 className="text-5xl font-extrabold mb-7">Oops!</h1>
+            <p className="text-xl">Sorry, Your Searching Page not Found.</p>
+            <br />
+            <Link to="/"><button className="btn bg-slate-300 font-bold">Back to Home</button></Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Home from "../pages/Home/Home";
 import Details from "../pages/Details/Details";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
           path: "/",
